8b10934aba625525534c586a54bef482e5c97694
mamd0uh <mamdouh.elmalah@gmail.com>
2022-10-31 04:59:59 -0400

    Added Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+
+export default function Footer() {
+  return (
+    <>
+    
+    <footer>
+        <p className="copy">&copy; Mamdouh Elmallah 2022 — Per Scholas </p>
+    </footer>
+    </>
+  )
+}
