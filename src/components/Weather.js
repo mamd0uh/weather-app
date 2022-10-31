68e591b23d5f64348767b10bbd61f3d58a285172
mamd0uh <mamdouh.elmalah@gmail.com>
2022-10-31 05:04:08 -0400

    Added Weekly Forecast (Not working yet)

---
+++ src/components/Weather.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import WeeklyWeather from "./WeeklyWeather/WeeklyWeather";
+
+export default function Weather() {
+
+const FORECAST_API = process.env.FORECAST_API;
+const locationKey = `349727_PC`;
+const [weatherInfo, setWeatherInfo] = useState
+
+  return (
+<div></div>
+  )
+}
---
+++ src/components/WeeklyWeather/WeeklyWeather.js
@@ -0,0 +1,8 @@
+export default function WeeklyWeather() {
+
+  return (
+    <div>
+
+    </div>
+  )
+}
